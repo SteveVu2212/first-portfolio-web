009c2266ed9ac1d8e14cf680710e75a5fa4fbe82
Steve Vu <stevevu2212@gmail.com>
2023-02-21 23:27:22 -0800

    upload files

---
+++ src/components/work/Data.jsx
@@ -0,0 +1,64 @@
+// import Work1 from "../../assets/work1.jpg";
+// import Work2 from "../../assets/work2.jpg";
+// import Work3 from "../../assets/work3.jpg";
+// import Work4 from "../../assets/work4.jpg";
+// import Work5 from "../../assets/work5.jpg";
+
+import house from "../../assets/house-rent.webp";
+import movie from "../../assets/movie.webp";
+import baseball from "../../assets/baseball.webp";
+import image_search from "../../assets/image-search.webp";
+import pension from "../../assets/pension.webp";
+
+export const projectsData = [
+  {
+    id: 1,
+    image: house,
+    title: "House Rental",
+    category: "web",
+    link: "https://github.com/SteveVu2212/org-rental-app",
+  },
+  {
+    id: 2,
+    image: movie,
+    title: "Movie Search",
+    category: "web",
+    link: "https://github.com/SteveVu2212/moviedb-app",
+  },
+  {
+    id: 3,
+    image: baseball,
+    title: "Brand design",
+    category: "modeling",
+    link: "https://github.com/SteveVu2212/Bayes_for_Baseball_analytics",
+  },
+  {
+    id: 4,
+    image: pension,
+    title: "Web design",
+    category: "dataviz",
+    link: "https://github.com/SteveVu2212/SteveVu2212.github.io",
+  },
+  {
+    id: 5,
+    image: image_search,
+    title: "App movil",
+    category: "modeling",
+    link: "https://github.com/SteveVu2212/Image-to-Image-Search",
+  },
+];
+
+export const projectsNav = [
+  {
+    name: "all",
+  },
+  {
+    name: "web",
+  },
+  {
+    name: "modeling",
+  },
+  {
+    name: "dataviz",
+  },
+];
