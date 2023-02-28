ecf1e26cfb9a6e6c08c45472cfefc989adb4dd0a
Steve Vu <stevevu2212@gmail.com>
2023-02-27 16:27:48 -0800

    new resume

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -5,46 +5,54 @@
 // import Work5 from "../../assets/work5.jpg";
 
 import house from "../../assets/house-rent.webp";
-import movie from "../../assets/movie.webp";
+import sprint_card from '../../assets/sprint-cards.png'
 import baseball from "../../assets/baseball.webp";
 import image_search from "../../assets/image-search.webp";
 import pension from "../../assets/pension.webp";
+import admin_dashboard from "../../assets/admin-dashboard.png";
 
 export const projectsData = [
   {
     id: 1,
     image: house,
-    title: "House Rental",
+    title: "House Rental Web",
     category: "web",
     link: "https://github.com/SteveVu2212/org-rental-app",
   },
   {
     id: 2,
-    image: movie,
-    title: "Movie Search",
+    image: sprint_card,
+    title: "Sprint Cards",
     category: "web",
-    link: "https://github.com/SteveVu2212/moviedb-app",
+    link: "https://github.com/SteveVu2212/sprint-cards",
   },
   {
     id: 3,
     image: baseball,
-    title: "Brand design",
+    title: "Baseball Analytics",
     category: "modeling",
     link: "https://github.com/SteveVu2212/Bayes_for_Baseball_analytics",
   },
   {
     id: 4,
     image: pension,
-    title: "Web design",
-    category: "dataviz",
+    title: "Pension Benchmark Modeling",
+    category: "modeling",
     link: "https://github.com/SteveVu2212/SteveVu2212.github.io",
   },
   {
     id: 5,
     image: image_search,
-    title: "App movil",
+    title: "Image Search Model",
     category: "modeling",
     link: "https://github.com/SteveVu2212/Image-to-Image-Search",
+  },
+  {
+    id: 6,
+    image: admin_dashboard,
+    title: "Admin Dashboard",
+    category: "dataviz",
+    link: "https://administration-dashboard.netlify.app/",
   },
 ];
 
